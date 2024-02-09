638d1f0cd2c0de644ab9dd9755ac4693a3947fb7
MeharbanSinghDhillon <152177076+MeharbanSinghDhillon@users.noreply.github.com>
2024-02-09 12:02:16 +0100

    Add files via upload

--- jasmeetmeharban/dir/script.js
+++ jasmeetmeharban/dir/script.js
@@ -5,4 +5,14 @@ let images="../photos/"+randomImages;
 
 (document.getElementById("center").src=`${images}`)
 }
-setInterval(loadImg,3000)
+setInterval(loadImg,3000);
+
+function loadImg1(){
+    let randomNumber= Math.floor(Math.random()*31+1);
+    console.log(randomNumber)
+let randomImages="jasmeet"+randomNumber+".jpg";
+let images="../photo/"+randomImages;
+
+(document.getElementById("center1").src=`${images}`)
+}
+setInterval(loadImg1,3000)
