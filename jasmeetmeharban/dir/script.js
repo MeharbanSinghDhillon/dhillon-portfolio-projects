2a1930ea63e27b936503f188500b7d21e14e9dcc
MeharbanSinghDhillon <152177076+MeharbanSinghDhillon@users.noreply.github.com>
2024-02-11 09:25:21 +0100

    Add files via upload

--- jasmeetmeharban/dir/script.js
+++ jasmeetmeharban/dir/script.js
@@ -1,3 +1,20 @@
+
+// $(document).ready(function(){
+//     $("#center").onload(function(){
+//         $("#center").slideToggle(100,
+//     function loadImg(){
+//         let randomNumber= Math.ceil(Math.random()*15);
+//     let randomImages="jasmeet"+randomNumber+".jpeg";
+//     let images="../photos/"+randomImages;
+    
+//     (document.getElementById("center").src=`${images}`)}
+
+    
+//       )})
+//      }
+//     );
+ 
+
 function loadImg(){
     let randomNumber= Math.ceil(Math.random()*15);
 let randomImages="jasmeet"+randomNumber+".jpeg";
@@ -6,6 +23,7 @@ let images="../photos/"+randomImages;
 (document.getElementById("center").src=`${images}`)
 }
 setInterval(loadImg,3000);
+
 
 function loadImg1(){
     let randomNumber= Math.floor(Math.random()*31+1);
@@ -16,3 +34,11 @@ let images="../photo/"+randomImages;
 (document.getElementById("center1").src=`${images}`)
 }
 setInterval(loadImg1,3000)
+
+$(document).ready(function(){
+$("#navbar").hide();
+$("#dropdown").click(function(){
+    $("#navbar").toggle("slow")
+})
+  
+  });
