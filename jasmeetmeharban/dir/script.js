9ada7b85d8976420d6fdda8f2159bf7301bd6baf
MeharbanSinghDhillon <152177076+MeharbanSinghDhillon@users.noreply.github.com>
2024-02-20 13:22:48 +0100

    Add files via upload

--- jasmeetmeharban/dir/script.js
+++ jasmeetmeharban/dir/script.js
@@ -15,30 +15,47 @@
 //     );
  
 
+let slideshow=document.getElementById("center");
+
+$(document).ready(function(){
+$("#btn").click(
+
 function loadImg(){
-    let randomNumber= Math.ceil(Math.random()*15);
-let randomImages="jasmeet"+randomNumber+".jpeg";
+
+let randomNumber= Math.ceil(Math.random()*25);
+let randomImages="jasmeetmeharban"+randomNumber+".JPEG";
 let images="../photos/"+randomImages;
 
 (document.getElementById("center").src=`${images}`)
-}
-setInterval(loadImg,3000);
 
 
-function loadImg1(){
-    let randomNumber= Math.floor(Math.random()*31+1);
-    console.log(randomNumber)
-let randomImages="jasmeet"+randomNumber+".jpg";
-let images="../photo/"+randomImages;
-
-(document.getElementById("center1").src=`${images}`)
-}
-setInterval(loadImg1,3000)
-
-$(document).ready(function(){
-$("#navbar").hide();
-$("#dropdown").click(function(){
-    $("#navbar").toggle("slow")
 })
+})
+
+
+
+
+ 
+
+
+
+
+
+
+// function loadImg1(){
+//     let randomNumber= Math.floor(Math.random()*31+1);
+//     console.log(randomNumber)
+// let randomImages="jasmeet"+randomNumber+".jpg";
+// let images="../photo/"+randomImages;
+
+// (document.getElementById("center1").src=`${images}`)
+// }
+// setInterval(loadImg1,3000)
+
+// $(document).ready(function(){
+// $("#navbar").hide();
+// $("#dropdown").click(function(){
+//     $("#navbar").toggle("slow")
+// })
   
-  });
+//   });
